42807ffc08ee7d94fbc04158df004dd33d40e758
willian santos <wsantosdelima44@gmail.com>
2023-03-28 17:57:26 -0300

    excercicio de exceção
    tratamento de exceção Java

---
+++ tratamentoDeExcecaoPratica/src/exceptions/BusinessException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class BusinessException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public BusinessException(String msg) {
+		super(msg);
+		}
+	
+	
+}
+
+
